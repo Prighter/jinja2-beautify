a97345a5b8b9350a5fa7b638625a9b04f00137b7
Hakan Alp <hakana926@gmail.com>
2021-12-05 20:43:16 +0300

    fix: inline and nested conditional statement issues solved

--- src/beautifyJinja.js
+++ src/beautifyJinja.js
@@ -3,15 +3,24 @@ import beautifier from "js-beautify";
 export function beautifyJinja(htmlString = "") {
   const serializedHTML = jinjaToHTML(htmlString);
   const beautifiedHTML = beautifier.html_beautify(serializedHTML);
-
   return HTMLToJinja(beautifiedHTML);
 }
 
 function jinjaToHTML(jinjaString = "") {
-  const r = /({%) *(.*?) +(.*?) *(%})/g;
-  return jinjaString.replace(r, (match, _, cmd) => {
+  const r = /({%|{%-}) *(.*?) +(.*?) *(-%}|%})/gs;
+  return jinjaString.replace(r, (match, _, cmd, __, ___,index) => {
+    for (let i = index; i > 0; i--) {
+      if(jinjaString[i] === ">")
+        break;
+      else if (jinjaString[i] === "<")
+        return match;
+    }
+    console.log(cmd)
     if (cmd === "from" || cmd === "extends" || cmd === "set") {
       return `<jinjaTag ${match}></jinjaTag>`;
+    } 
+    if (cmd == "else" || cmd == "elif") {
+      return `</jinjaTag><jinjaTag ${match}>`;
     }
     if (cmd.startsWith("end")) {
       return `</jinjaTag ${match}>`;
@@ -22,16 +31,16 @@ function jinjaToHTML(jinjaString = "") {
 
 function HTMLToJinja(htmlString = "") {
   // eslint-disable-next-line prefer-regex-literals
-  const r = new RegExp("(<jinjaTag|</jinjaTag) *(.*?) +(.*?) *(>|/>)", "g");
+  const r = new RegExp("(<jinjaTag|</jinjaTag) *(.*?) +(.*?) *(>|/>)", "gs");
   return htmlString
     .replace(r, (match) => {
-      const tempMatch = match.includes("</")
-        ? match.replace("</jinjaTag ", "")
-        : match.replace("<jinjaTag ", "");
+      let tempMatch = match.replace(/<\/jinjaTag>/g, "");
+      tempMatch = tempMatch.includes("</")
+        ? tempMatch.replace(/<\/jinjaTag /g, "")
+        : tempMatch.replace(/<jinjaTag /g, "");
 
       return tempMatch[tempMatch.length - 1] === ">"
         ? tempMatch.slice(0, tempMatch.length - 1)
         : tempMatch;
     })
-    .replace(/<\/jinjaTag>/g, "");
 }
