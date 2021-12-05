c1277f583c8d5d14c4d7bcafcb7f3ac057bf7258
Hakan Alp <hakana926@gmail.com>
2021-12-05 15:57:28 +0300

    fix: npm publish issues solved

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+#!/usr/bin/env node
+
 import Yargs from "yargs";
-import { beautifyJinjaFile } from "./src/app";
+import { beautifyJinjaFile } from "./src/app.js";
 
 const args = Yargs(process.argv.slice(2)).argv;
 
--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import fs from "fs";
-import { beautifyJinja } from "./beautifyJinja";
+import { beautifyJinja } from "./beautifyJinja.js";
 
 export function beautifyJinjaFile(file = "") {
   fs.readFile(file, "utf8", (readError, code) => {
