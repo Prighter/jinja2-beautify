0f4e4d0188f064853a03c5a34a610c4707c4a83b
Hakan Alp <hakan@prighter.com>
2022-01-31 13:18:06 +0300

    test mirror

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import { beautifyJinja } from "./beautifyJinja.js";
 export function beautifyJinjaFile(file = "") {
   const beautifiedFile = formatFile(file);
   fs.writeFileSync(file, beautifiedFile);
-}
+} 
 
 export function formatFile(file = "") {
   const html = fs.readFileSync(file, "utf8");
